6a14602f759981e038f73bf948f6de8477c16842
framaz <framaz@yandex.ru>
2019-04-19 11:20:03 +0500

    better admining

--- helpdesk/models.py
+++ helpdesk/models.py
@@ -6,6 +6,8 @@ from django.db.models.functions import ExtractDay, ExtractMonth, ExtractYear
 class Department(models.Model):
     name = models.CharField(max_length=100)
     head = models.ForeignKey('auth.User',null=True,on_delete=models.CASCADE, related_name="head")
+    def __str__(self):
+        return self.name+" "+str(self.id)
 class Ticket(models.Model):
     requestor = models.ForeignKey('auth.User',on_delete=models.CASCADE, related_name="requestor")
     worker = models.ForeignKey('auth.User',on_delete=models.SET_NULL, null=True, related_name="worker")
@@ -19,14 +21,20 @@ class Ticket(models.Model):
     finished = models.BooleanField(default="False")
     deleted = models.BooleanField(default="False")
     chat = models.TextField(default="")
+    def __str__(self):
+        return self.theme+" "+str(self.id)
     def nice_exp_date(this):
         return ExtractDay(this.exp_date)+"/"+ExtractMonth(this.exp_date)+"/"+ExtractYear(this.exp_date)
 class EmployeesInDepartments(models.Model):
     person = models.ForeignKey('auth.User',on_delete=models.CASCADE, related_name="person")
     department = models.ForeignKey('Department',on_delete=models.CASCADE, related_name="department")
+    def __str__(self):
+        return self.person.first_name+self.person.last_name+" X "+self.department.name+" "+str(self.id)
 class QuestionsAndAnswers(models.Model):
     question = models.CharField(max_length=1000,default="")
     answer = models.CharField(max_length=1000,default="")
+    def __str__(self):
+        return self.question+" "+str(self.id)
 class Message(models.Model):
     sender = models.ForeignKey('auth.User',on_delete=models.CASCADE,related_name="sender")
     reciver = models.ForeignKey('auth.User',on_delete=models.CASCADE,related_name="reciver")
